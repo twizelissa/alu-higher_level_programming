9c8771c6bcc849d764cfc0698a80ccefb6f1f798
twizelissa <twizelissa@gmail.com>
2024-03-20 01:05:10 +0200

    fixing PEP8

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -16,3 +16,4 @@ with urllib.request.urlopen(req) as response:
     print("\t- type:", type(content))
     print("\t- content:", content)
     print("\t- utf8 content:", content.decode("utf-8"))
+    
