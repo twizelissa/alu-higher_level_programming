e0e24e168299028477bef7b954581afa492fc162
twizelissa <twizelissa@gmail.com>
2024-03-24 21:19:48 +0200

    fixing question 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Python script that fetchs."""
-
-
+"""
+Python script that fetches https://alu-intranet.hbtn.io/status
+"""
 import requests
-if __name__ == "__main__":
-    requ = requests.get('https://intranet.hbtn.io/status')
+
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(requ.text)))
-    print("\t- content: {}".format(requ.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
